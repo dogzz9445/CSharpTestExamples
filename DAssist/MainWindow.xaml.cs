78357e05c500e51e3516e79336096ea0b67e82b3
dmjang <dogzz9445@gmail.com>
2021-02-04 17:16:46 +0900

    Add tray to DAssist

--- DAssist/MainWindow.xaml.cs
+++ DAssist/MainWindow.xaml.cs
@@ -25,21 +25,48 @@ namespace DAssist
         {
             InitializeComponent();
 
-            // 윈도우 어디를 선택해도 이동 가능
-            this.MouseLeftButtonDown += new MouseButtonEventHandler((sender, e) => this.DragMove());
-            // 메인 메뉴 종료 클릭시 종료
-            this.MainMenuItemClose.Click += new RoutedEventHandler((sender, e) => this.Close());
+            this.Loaded += (sender, args) =>
+            {
+                /////////////////
+                //
+                // UI
+                //
+                /////////////////
+                
+                // 윈도우 창 위치
+                var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+                Left = desktopWorkingArea.Right - this.Width;
+                Top = desktopWorkingArea.Bottom - this.Height;
+                // 아이콘 초기화
+                //this.Icon = new System.Drawing.Icon(@"Graphicloads.ico");
+                TrayTaskbarIcon.Icon = new System.Drawing.Icon(@"Graphicloads.ico");
+
+
+                /////////////////
+                //
+                // 이벤트
+                //
+                /////////////////
+
+                // 윈도우 어디를 선택해도 이동 가능
+                MouseLeftButtonDown += (s, e) => this.DragMove();
+                // 응용프로그램 종료시 아이콘 제거
+                System.Windows.Application.Current.Exit += (s, e) => TrayTaskbarIcon.Dispose();
+                // 메인 팝업 메뉴 트레이 클릭시 / 윈도우 비활성화
+                MainMenuItemTray.Click += (s, e) => this.Hide();
+                // 메인 팝업 메뉴 종료 클릭시 / 응용프로그램 종료
+                MainMenuItemClose.Click += (s, e) => System.Windows.Application.Current.Shutdown();
+                // 메인 메뉴 트레이 클릭시 / 윈도우 비활성화
+                ButtonTray.Click += (s, e) => this.Hide();
+                // 아이콘 더블클릭시 / 윈도우 활성화
+                TrayTaskbarIcon.TrayMouseDoubleClick += (s, e) => this.Show();
+                // 태스크바 아이콘 설정버튼 클릭시 / 윈도우 활성화
+                (TrayTaskbarIcon.ContextMenu.Items.GetItemAt(1) as MenuItem).Click += (s, e) => this.Show();
+                // 태스크바 아이콘 종료버튼 클릭시 / 응용프로그램 종료
+                (TrayTaskbarIcon.ContextMenu.Items.GetItemAt(3) as MenuItem).Click += (s, e) => System.Windows.Application.Current.Shutdown();
+            };
         }
-
-        private void WindowLoaded(object sender, RoutedEventArgs e)
-        {
-            var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
-            this.Left = desktopWorkingArea.Right - this.Width;
-            this.Top = desktopWorkingArea.Bottom - this.Height;
-
-            ContextMenu menu = new ContextMenu();
-        }
-
+            
         private void OnCopy(object sender, ExecutedRoutedEventArgs e)
         {
             if (e.Parameter is string stringValue)
--- RampApp/RampWindow.xaml.cs
+++ RampApp/RampWindow.xaml.cs
@@ -5,38 +5,38 @@ namespace Ramp
 {
     public partial class RampWindow : Window
     {
-        private RampDisplay displayRamp;
+        //private RampDisplay displayRamp;
 
         public RampWindow()
         {
             InitializeComponent();
 
-            displayRamp = new RampDisplay();
-            sliderBrightness.ValueChanged += this.SliderBrightness_ValueChanged;
-            sliderGamma.ValueChanged += this.SliderGamma_ValueChanged;
+            //displayRamp = new RampDisplay();
+            //sliderBrightness.ValueChanged += this.SliderBrightness_ValueChanged;
+            //sliderGamma.ValueChanged += this.SliderGamma_ValueChanged;
         }
 
-        private void SliderBrightness_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            displayRamp.SetBrightness((sender as Slider).Value);
-        }
+        //private void SliderBrightness_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        //{
+        //    displayRamp.SetBrightness((sender as Slider).Value);
+        //}
 
-        private void SliderGamma_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            displayRamp.SetGamma((sender as Slider).Value);
-        }
+        //private void SliderGamma_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        //{
+        //    displayRamp.SetGamma((sender as Slider).Value);
+        //}
 
-        private void ButtonApply_Clicked(object sender, RoutedEventArgs e)
-        {
-            displayRamp.SetBrightness(sliderBrightness.Value);
-            displayRamp.SetGamma(sliderGamma.Value);
-            this.Close();
-        }
+        //private void ButtonApply_Clicked(object sender, RoutedEventArgs e)
+        //{
+        //    displayRamp.SetBrightness(sliderBrightness.Value);
+        //    displayRamp.SetGamma(sliderGamma.Value);
+        //    this.Close();
+        //}
 
-        private void ButtonCancel_Clicked(object sender, RoutedEventArgs e)
-        {
-            displayRamp.UpdateRamps(true);
-            this.Close();
-        }
+        //private void ButtonCancel_Clicked(object sender, RoutedEventArgs e)
+        //{
+        //    displayRamp.UpdateRamps(true);
+        //    this.Close();
+        //}
     }
 }
